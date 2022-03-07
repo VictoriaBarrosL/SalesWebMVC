ad50030938b420eb470532e2491fa5a879752a85
Victoria Barros <lis.victoriab@gmail.com>
2022-03-07 20:08:52 -0300

    Implementing grouping search Error

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -13,8 +13,7 @@ namespace SalesWebMVC.Models
         public DateTime Date { get; set; }
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
-        public Seller Seller { get; }
-        public Seller seller { get; set; }
+        public Seller Seller { get; set; }
 
         public SalesRecord()
         {
